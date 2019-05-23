3ba21190c98f04e7a2072a9da1e972e9d8461ef3
Tohin666 <tohin666@gmail.com>
2019-05-23 07:57:05 +0300

    added db credentials

--- web/index.php
+++ web/index.php
@@ -1,8 +1,10 @@
 <?php
 
-// comment out the following two lines when deployed to production
-defined('YII_DEBUG') or define('YII_DEBUG', true);
-defined('YII_ENV') or define('YII_ENV', 'dev');
+// если выкладываем на heroku, то переключаем dev среду на prod
+//if (! $_SERVER['SERVER_NAME'] == "morning-island-64245.herokuapp.com") {
+//    defined('YII_DEBUG') or define('YII_DEBUG', true);
+//    defined('YII_ENV') or define('YII_ENV', 'dev');
+//}
 
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
