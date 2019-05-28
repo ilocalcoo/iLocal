fdbe766d9a4102dacfb08f9986952fdf7acb0077
Yarcrazy <yacrazy@yandex.ru>
2019-05-29 01:07:03 +0700

    При изменении таблицы мешал фк на другую таблицу Исправил, конечно, некрасиво, но работает

--- migrations/m190524_131442_create_shopPhoto_table.php
+++ migrations/m190524_131442_create_shopPhoto_table.php
@@ -12,6 +12,8 @@ class m190524_131442_create_shopPhoto_table extends Migration
      */
     public function safeUp()
     {
+        $this->dropForeignKey('fx_shop_shopPhoto', 'shop');
+        $this->dropTable('{{%shopPhoto}}');
         $this->createTable('{{%shopPhoto}}', [
             'id' => $this->primaryKey(),
             'shopId' => $this->integer(),
@@ -25,5 +27,19 @@ class m190524_131442_create_shopPhoto_table extends Migration
     public function safeDown()
     {
         $this->dropTable('{{%shopPhoto}}');
+        $this->createTable('{{%shopPhoto}}', [
+            'id' => $this->primaryKey(),
+            'shopPhoto1' => $this->string(),
+            'shopPhoto2' => $this->string(),
+            'shopPhoto3' => $this->string(),
+            'shopPhoto4' => $this->string(),
+            'shopPhoto5' => $this->string(),
+            'shopPhoto6' => $this->string(),
+            'shopPhoto7' => $this->string(),
+            'shopPhoto8' => $this->string(),
+            'shopPhoto9' => $this->string(),
+            'shopPhoto10' => $this->string(),
+        ]);
+        $this->addForeignKey('fx_shop_shopPhoto', 'shop', ['shopPhotoId'], 'shopPhoto', ['id']);
     }
 }
