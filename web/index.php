c9e47c9cb6a85494b606dbd112d9dabbc5fc9435
Tohin666 <tohin666@gmail.com>
2019-06-07 17:18:50 +0300

    heroku

--- web/index.php
+++ web/index.php
@@ -2,9 +2,10 @@
 
 // если выкладываем на heroku, то переключаем dev среду на prod
 if (! ($_SERVER['SERVER_NAME'] == "morning-island-64245.herokuapp.com")) {
-    defined('YII_DEBUG') or define('YII_DEBUG', true);
+
     defined('YII_ENV') or define('YII_ENV', 'dev');
 }
+defined('YII_DEBUG') or define('YII_DEBUG', true);
 
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
