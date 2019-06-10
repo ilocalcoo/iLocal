b490dd0e7cd56843e95271915b22481f77fe1225
Yarcrazy <yacrazy@yandex.ru>
2019-06-11 01:16:01 +0700

    продолжаем мудрить с лентой

--- views/event/index.php
+++ views/event/index.php
@@ -7,8 +7,9 @@ use yii\widgets\Pjax;
 /* @var $dataProvider yii\data\ActiveDataProvider */
 /* @var \app\models\search\EventSearch $searchModel */
 /* @var \app\models\Event $shortDescData */
-/* @var \app\models\Event[] $models */
+/* @var \app\models\Event $event */
 /* @var \app\models\EventPhoto $photo */
+/* @var \app\models\Shop[] $shops */
 
 $this->title = 'Events';
 $this->params['breadcrumbs'][] = $this->title;
@@ -16,53 +17,70 @@ $this->params['breadcrumbs'][] = $this->title;
 <div class="event-index">
 
     <?php Pjax::begin(); ?>
-    <?php
-    foreach ($models as $model) { ?>
 	<main class="container">
+	<?php
+    foreach ($shops as $shop) { ?>
+
 		<div class="content">
 			<div class="cont_title">
 				<div class="shop_img">
-					<img src="<?= $model->eventOwner->getShopPhotos->limit(1)->shopPhoto ?>"
-						 alt="<?= $model->eventOwner->shopShortName ?>">
+					<img
+						src="<?php echo $shop->getShopPhotos()->limit(1)->asArray()->all()[0]['shopPhoto'] ?>"
+						alt="<?= $shop->shopShortName ?>"/>
 				</div>
 				<div class="right_title">
 					<div class="shop_name">
-						<?= $model->eventOwner->shopShortName ?>
+                        <?= $shop->shopShortName ?>
 					</div>
 					<div class="shop_address">
-						<?php
-							$address = $model->eventOwner->shopAddress->city . ', ул. ' .
-							$model->eventOwner->shopAddress->street . ', д. ' .
-							$model->eventOwner->shopAddress->houseNumber . ', к. ' .
-							$model->eventOwner->shopAddress->housing . ', к. ' .
-						?>
+                        <?php
+                        $address = $shop->shopAddress->city . ', ул. ' .
+                            $shop->shopAddress->street . ', д. ' .
+                            $shop->shopAddress->houseNumber;
+                        // TODO доделать отображение корпусов и строений
+                        echo $address;
+                        ?>
 					</div>
 				</div>
 			</div>
 			<div class="carousel">
-        <?php
-        $photos = $model->getTopEventPhotos()->asArray()->all();
-        if (count($photos) == 0) {
-            $photos = [
-                0 => [
-                    'eventPhoto' => '/img/nophoto.jpg'
-                ]
-            ];
-        }
-        $items = [];
-        foreach ($photos as $photo) {
-            $content = '<img src="' . $photo['eventPhoto'] . '"/img>';
-            array_push($items, $content);
-        }
-        echo Carousel::widget([
-            'items' => $items
-        ]);
-        ?>
-        <?php } ?>
+                <?php
 
-        <?php Pjax::end(); ?>
-
+				foreach ($shop->getEvents()->all() as $event) {
+                    $photos = $event->getTopEventPhotos()->asArray()->all();
+                    if (count($photos) == 0) {
+                        $photos = [
+                            0 => [
+                                'eventPhoto' => '/img/nophoto.jpg'
+                            ]
+                        ];
+                    }
+                    $items = [];
+                    foreach ($photos as $photo) {
+                        $content = '<div class="event_card">
+							<div class="card_top">' .
+                            Carousel::widget([
+                                'items' => '<img src="' . $photo['eventPhoto'] . '"/img>',
+                            ]) .
+                            '</div>
+							<div class="card_bot">
+								
+							</div>
+						</div>';
+                        $content .= '<img src="' . $photo['eventPhoto'] . '"/img>';
+                        array_push($items, $content);
+                    }
+                    echo Carousel::widget([
+                        'items' => $items
+                    ]);
+                }
+                ?>
 			</div>
 		</div>
+                <?php } ?>
+
+                <?php Pjax::end(); ?>
+
+
 	</main>
 </div>
