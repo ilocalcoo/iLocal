a94610a4d36e05dcfa5a4c5dad0d0670c367a7b5
Иван <yarcrazy@yandex.ru>
2019-09-06 00:46:06 +0700

    авторизованные юзеры могут заходить в свой кабинет все же)

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -18,45 +18,50 @@ class UserController extends Controller
   public function behaviors()
   {
     return [
-        'access' => [
-            'class' => AccessControl::className(),
-            'rules' => [
-                [
-                    'allow' => false,
-                    'actions' => ['business'],
-                    'roles' => ['?'],
-                ],
-            ],
+      'access' => [
+        'class' => AccessControl::className(),
+        'rules' => [
+          [
+            'allow' => false,
+            'actions' => ['business'],
+            'roles' => ['?'],
+          ],
+          [
+            'allow' => true,
+            'roles' => ['@'],
+          ],
         ],
+      ],
     ];
   }
-    /**
-     * @return string
-     */
-    public function actionBusiness()
-    {
-        $userShops = Shop::find()
-            ->where(['=', 'creatorId', Yii::$app->user->id])
-            ->joinWith(Shop::RELATION_SHOP_PHOTOS)
-            ->asArray()
-            ->all();
 
-        $userEvents = Event::find()
-            ->where(['=', 'creatorId', Yii::$app->user->id])
-            ->joinWith(Event::RELATION_EVENT_PHOTOS)
-            ->asArray()
-            ->all();
+  /**
+   * @return string
+   */
+  public function actionBusiness()
+  {
+    $userShops = Shop::find()
+      ->where(['=', 'creatorId', Yii::$app->user->id])
+      ->joinWith(Shop::RELATION_SHOP_PHOTOS)
+      ->asArray()
+      ->all();
 
-        $userHappenings = Happening::find()
-            ->where(['=', 'creatorId', Yii::$app->user->id])
-            ->joinWith(Happening::RELATION_HAPPENING_PHOTOS)
-            ->asArray()
-            ->all();
+    $userEvents = Event::find()
+      ->where(['=', 'creatorId', Yii::$app->user->id])
+      ->joinWith(Event::RELATION_EVENT_PHOTOS)
+      ->asArray()
+      ->all();
 
-        return $this->render('business', [
-            'userShops' => $userShops,
-            'userEvents' => $userEvents,
-            'userHappenings' => $userHappenings,
-        ]);
-    }
+    $userHappenings = Happening::find()
+      ->where(['=', 'creatorId', Yii::$app->user->id])
+      ->joinWith(Happening::RELATION_HAPPENING_PHOTOS)
+      ->asArray()
+      ->all();
+
+    return $this->render('business', [
+      'userShops' => $userShops,
+      'userEvents' => $userEvents,
+      'userHappenings' => $userHappenings,
+    ]);
+  }
 }
