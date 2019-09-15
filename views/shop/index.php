dcb99d7ecb80e024db6f27ac330f3bc810a31f3d
Yarcrazy <yacrazy@yandex.ru>
2019-09-15 18:20:30 +0700

    закончил поле поиска для хэдера мобильной версии

--- views/shop/index.php
+++ views/shop/index.php
@@ -199,3 +199,11 @@ $this->title = $type . ' рядом с вами';
 </div>
 <a href="" id="loupe" class="d-md-none d-sm-block"></a>
 <a id="loupe-tail" class="d-md-none d-sm-block"></a>
+<?php if (!array_key_exists('shopShortName', $_GET)) { ?>
+	<div id="find-placeholder">
+		<div class="loupe-img"></div>
+		<div class="loupe-tail-img"></div>
+		<span>Поиск...</span>
+	</div>
+<?php } ?>
+<img src="img/main/close.svg" class="nav-link" id="find-close" alt="find-close" width="32px" height="32px">
--- web/js/shop_feed.js
+++ web/js/shop_feed.js
@@ -6,8 +6,22 @@ $(document).ready(function () {
     history.pushState('','','/shops');
     location.reload();
   });
+
   $('#loupe').click(function (event) {
     event.preventDefault();
     $('.shop-find').css('visibility', 'visible');
+    $('#find-close').show();
+    $('#find-placeholder').css('display', 'flex');
   });
+
+  $('#find-close').click(function (event) {
+    event.preventDefault();
+    $('.shop-find').css('visibility', 'hidden');
+    $('#find-close').hide();
+    $('#find-placeholder').hide();
+  });
+
+  $('.shop-find').on('input', function () {
+    $('#find-placeholder').hide();
+  })
 });
