cdd6381cc5a0bb05906aecb12860999fb8ef345a
jerrygacket@gmail.com <jerrygacket@gmail.com>
2019-09-18 22:47:23 +0300

    Места на странице бизнесу

---
+++ assets/BusinessAsset.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @link http://www.yiiframework.com/
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license http://www.yiiframework.com/license/
+ */
+
+namespace app\assets;
+
+use yii\web\AssetBundle;
+
+/**
+ * Main page application asset bundle.
+ */
+class BusinessAsset extends AssetBundle
+{
+  public $basePath = '@webroot';
+  public $baseUrl = '@web';
+  public $css = [
+    'css/user/business.css',
+  ];
+  public $js = [
+  ];
+  public $depends = [
+    'app\assets\AppAsset',
+    'yii\web\YiiAsset',
+    'yii\bootstrap4\BootstrapAsset',
+  ];
+}
