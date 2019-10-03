bdb7458b516b86661b71e7cb346419bbdc6368b3
Yarcrazy <yacrazy@yandex.ru>
2019-10-03 20:05:37 +0700

    начинаю расставлять для деска

--- views/site/about.php
+++ views/site/about.php
@@ -7,70 +7,71 @@ use app\assets\AboutAsset;
 AboutAsset::register($this);
 ?>
 
-	<div class="pink">
-		<div class="row justify-content-center">
-			<h3>Места, акции и события рядом с тобой</h3>
-		</div>
-		<div class="row justify-content-center">
-			<img src="img/about/notebook.png" alt="notebook" class="notebook">
-		</div>
-		<div class="row text-center">
-			I’m Local помогает узнавать о местах, акциях и событиях поблизости напрямую от местного малого бизнеса и
-			организаторов мероприятий. Это сервис для коммуникации между локальным бизнесом и людьми поблизости.
+<div class="row justify-content-center">
+	<h3>Места, акции и события рядом с тобой</h3>
+</div>
+<div class="row justify-content-center">
+	<img src="img/about/notebook.png" alt="notebook" class="notebook">
+</div>
+<div class="row text-center">
+	I’m Local помогает узнавать о местах, акциях и событиях поблизости напрямую от местного малого бизнеса и
+	организаторов мероприятий. Это сервис для коммуникации между локальным бизнесом и людьми поблизости.
+</div>
+<div class="row">
+	<div class="col-md-6 col-12">
+		<h2 class="text-center">Узнавай первым!</h2>
+		<br>
+		<div class="text-center">Открывается рядом магазин, детский центр или аптека? Ты узнаешь об этом первым!</div>
+	</div>
+	<div class="col-md-6 col-12 phone">
+		<img src="img/about/body.png" alt="body" class="phone-body">
+		<img src="img/about/screen1.png" alt="screen" class="phone-screen">
+	</div>
+</div>
+<div class="row">
+	<div class="col-md-6 col-12 phone">
+		<img src="img/about/body.png" alt="body" class="phone-body">
+		<img src="img/about/screen2.png" alt="screen" class="phone-screen">
+	</div>
+	<div class="col-md-6 col-12">
+		<h2 class="text-center">Загляни в афишу событий</h2>
+		<br>
+		<div class="text-center">Городской фестиваль или ярмарка на соседнем бульваре, бесплатные пробные занятия твоего
+			любимого
+			вида
+			спорта или экскурсия с биологом в парке - следи за событиями для всей семьи.
 		</div>
 	</div>
-	<div class="row">
-		<div class="col-md-6 col-12">
-			<h2 class="text-center">Узнавай первым!</h2>
-			<br>
-			<div class="text-center">Открывается рядом магазин, детский центр или аптека? Ты узнаешь об этом первым!</div>
-		</div>
-		<div class="col-md-6 col-12 phone">
-			<img src="img/about/body.png" alt="body" class="phone-body">
-			<img src="img/about/screen1.png" alt="screen" class="phone-screen">
-		</div>
-	</div>
-	<div class="row">
-		<div class="col-md-6 col-12 phone">
-			<img src="img/about/body.png" alt="body" class="phone-body">
-			<img src="img/about/screen2.png" alt="screen" class="phone-screen">
-		</div>
-		<div class="col-md-6 col-12">
-			<h2 class="text-center">Загляни в афишу событий</h2>
-			<br>
-			<div class="text-center">Городской фестиваль или ярмарка на соседнем бульваре, бесплатные пробные занятия твоего любимого
-				вида
-				спорта или экскурсия с биологом в парке - следи за событиями для всей семьи.
-			</div>
+</div>
+<div class="row">
+	<div class="col-md-6 col-12">
+		<h2 class="text-center">Выбирай! Не пропускай!</h2>
+		<br>
+		<div class="text-center">Не хочешь пропустить большую распродажу детских товаров или камерную дегустацию c выгодными
+			предложениями
+			в винотеке поблизости? Акции подскажут, как с пользой распорядиться временем и деньгами.
 		</div>
 	</div>
-	<div class="row">
-		<div class="col-md-6 col-12">
-			<h2 class="text-center">Выбирай! Не пропускай!</h2>
-			<br>
-			<div class="text-center">Не хочешь пропустить большую распродажу детских товаров или камерную дегустацию c выгодными
-				предложениями
-				в винотеке поблизости? Акции подскажут, как с пользой распорядиться временем и деньгами.
-			</div>
-		</div>
-		<div class="col-md-6 col-12 phone">
-			<img src="img/about/body.png" alt="body" class="phone-body">
-			<img src="img/about/screen3.png" alt="screen" class="phone-screen">
-		</div>
+	<div class="col-md-6 col-12 phone">
+		<img src="img/about/body.png" alt="body" class="phone-body">
+		<img src="img/about/screen3.png" alt="screen" class="phone-screen">
 	</div>
-	<div class="row justify-content-center">
-		<div class="col-md-8 col-12">
-			<h2 class="text-center">И ехать никуда не надо!</h2>
-			<br>
-			<div class="text-center">Задача платформы – сделать время, проведенное рядом с домом, комфортным и насыщенным, развивать
-				инфраструктуру, помогая малому бизнесу быть востребованным и создавать качественные предложения
-				для людей поблизости.
-			</div>
+</div>
+<div class="row justify-content-center">
+	<div class="col-md-8 col-12">
+		<h2 class="text-center">И ехать никуда не надо!</h2>
+		<br>
+		<div class="text-center">Задача платформы – сделать время, проведенное рядом с домом, комфортным и насыщенным,
+			развивать
+			инфраструктуру, помогая малому бизнесу быть востребованным и создавать качественные предложения
+			для людей поблизости.
 		</div>
 	</div>
-	<div class="thanks text-center">
-		<h3>Пройди опрос!</h3>
-		<h3>Спасибо!</h3>
-		<div>Есть, что сказать или возникли вопросы? Будем рады!</div>
-		<div>hello@imlocal.ru</div>
-	</div>
+</div>
+<div class="thanks text-center">
+	<h3>Пройди опрос!</h3>
+	<h3>Спасибо!</h3>
+	<div>Есть, что сказать или возникли вопросы? Будем рады!</div>
+	<div>hello@imlocal.ru</div>
+</div>
+<div class="back"></div>
