dde1a21f0f2ee215c7fa8eb0846636136805ba0d
jerrygacket <jerrygacket@gmail.com>
2019-10-25 10:28:43 +0300

    добавил апи контроллеры для удаления фотографий мест, акций, событий

---
+++ modules/api/controllers/HappeningphotoController.php
@@ -0,0 +1,33 @@
+<?php
+
+
+namespace app\modules\api\controllers;
+
+
+use yii\filters\auth\HttpBasicAuth;
+use yii\rest\ActiveController;
+
+class HappeningphotoController extends ActiveController
+{
+    public $modelClass = 'app\models\HappeningPhoto';
+
+    public function behaviors()
+    {
+        $behaviors = parent::behaviors();
+        // Добавляем атунтификацию через BasicAuth. Токен доступа отправляется как имя пользователя.
+        $behaviors['authenticator'] = [
+            'class' => HttpBasicAuth::className(),
+            // Отключаем аутентификацию при запросе магазинов через GET.
+            'except' => ['index', 'view'],
+        ];
+        return $behaviors;
+    }
+
+    public function actions()
+    {
+        $actions = parent::actions();
+        unset($actions['create']);
+        return $actions;
+    }
+
+}
