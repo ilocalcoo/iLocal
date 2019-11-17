a82b9aeaa99a97c4894ba76f7d346fbc2e3361f8
Yarcrazy <yacrazy@yandex.ru>
2019-11-18 00:23:59 +0700

    доработка

--- config/web.php
+++ config/web.php
@@ -131,7 +131,6 @@ $config = [
         ['class' => 'yii\rest\UrlRule', 'controller' => 'api/eventphoto'],
         ['class' => 'yii\rest\UrlRule', 'controller' => 'api/image'],
         ['class' => 'yii\rest\UrlRule', 'controller' => 'api/userAddress'],
-        'api/<controller:(user)>s/<id:\d+>/<action:(view|update|delete)>' => '<controller>/<action>',
       ],
     ],
   ],
--- modules/api/controllers/UserAddressController.php
+++ modules/api/controllers/UserAddressController.php
@@ -9,22 +9,20 @@ class UserAddressController extends ActiveController
 {
   public $modelClass = 'app\models\UserAddress';
 
-//  public function behaviors()
-//  {
-//    $behaviors = parent::behaviors();
-//    // Добавляем аутентификацию через BasicAuth. Токен доступа отправляется как имя пользователя.
-//    $behaviors['authenticator'] = [
-//      'class' => HttpBasicAuth::className(),
-//      // Отключаем аутентификацию при запросе токена.
-//      'except' => ['view'],
-//    ];
-//    return $behaviors;
-//  }
+  public function behaviors()
+  {
+    $behaviors = parent::behaviors();
+    // Добавляем аутентификацию через BasicAuth. Токен доступа отправляется как имя пользователя.
+    $behaviors['authenticator'] = [
+      'class' => HttpBasicAuth::className(),
+    ];
+    return $behaviors;
+  }
 
   public function actions()
   {
     $actions = parent::actions();
-//    unset($actions['index']);
+    unset($actions['index']);
     return $actions;
   }
 }
